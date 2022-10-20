515d3c12606bb33644563e1af869dedecfcf688a
abishekGithub <abishekshah12315@gmail.com>
2022-10-21 04:02:38 +0545

    first commit

---
+++ src/components/ToDo.js
@@ -0,0 +1,129 @@
+import { useState,useEffect } from "react"
+import { BsPlus } from "react-icons/bs";
+import { BsPencilSquare } from "react-icons/bs";
+import { MdDelete } from "react-icons/md"
+
+const getLocalData = () =>{
+    const lists = localStorage.getItem("mytodolist");
+
+    if(lists){
+        return JSON.parse(lists)
+    }else{
+        return [];
+    }
+}
+
+const ToDo = () => {
+
+    const [InputData, setInputData] = useState()
+    const [items, setItems] = useState(getLocalData())
+    const [isEditedItem,setIsEditedItem] = useState("")
+    const [toggleButton,setToggleButton] = useState(false)
+
+    const InputValue = () => {
+        if (!InputData) {
+            alert("plzz fill the value")
+        } else {
+            const myNewInputData = {
+                id: new Date().getTime().toString(),
+                name: InputData
+            }
+            setItems([...items, myNewInputData]);
+            setInputData("");
+            
+        }
+    }
+
+    const editItem = (index) =>{
+        const edit_items = items.find((curElem) => {
+            return curElem.id === index;
+        })
+
+        setInputData(edit_items.name);
+        setIsEditedItem(index)  
+        setToggleButton(true)
+    }
+
+    const DeleteItems = (index) =>{
+
+        const updatedItems = items.filter((curElem)=>{
+            return curElem.id !== index;
+        })
+        setItems(updatedItems)
+
+    }
+
+
+    const removeAll = () =>{
+        setItems([])
+    }
+
+    useEffect(()=>{
+        localStorage.setItem("mytodolist",JSON.stringify(items))
+    },[items])
+
+
+    return (
+        <div className="todo">
+
+
+            <div className="container">
+
+                <div className="title">ToDo <span>List</span> </div>
+
+
+                <div className="box1">
+                    <p className="emoji">✍️</p>
+                    <input type="text"
+                        className="no-outline"
+                        placeholder="  Add Item .."
+                        value={InputData}
+                        onChange={(event) => setInputData(event.target.value)}
+                    />
+                    {
+                        toggleButton ? (<p className="plusicon"
+                        onClick={InputValue}
+                    ><BsPencilSquare /></p>) : (<p className="plusicon"
+                        onClick={InputValue}
+                    ><BsPlus /></p>)
+                    }
+                </div>
+
+                {
+                    items.map((curElem, index) => {
+                        return (
+                            <>
+                                <div className="box2">
+                                    <p className="textitem" key={curElem.id}>{curElem.name}</p>
+                                    <input type="text"
+                                        className="no-outline"
+                                    />
+                                    <p className="pencilicon"
+                                    onClick={()=> editItem(curElem.id)}
+                                    ><BsPencilSquare /></p>
+                                    <p className="plusicon"
+                                    onClick={() => DeleteItems(curElem.id)}
+                                    ><MdDelete /></p>
+                                </div>
+                            </>
+                        )
+
+                    })
+                }
+
+
+                <div className="btn2">
+                    <button
+                    onClick={removeAll}
+                    >CHECK LIST</button>
+                </div>
+
+
+            </div>
+        </div>
+
+    )
+
+}
+
+export default ToDo
